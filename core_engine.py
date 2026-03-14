ca9d767591a5fd07a72ef47150b1298314e7b593
PenguinMeaN <nikto2607@gmail.com>
2026-03-14 13:46:51 +0300

    Create core_engine.py

---
+++ core_engine.py
@@ -0,0 +1,9 @@
+# --- ТВОИ НАСТРОЙКИ ---
+GITHUB_USER = "PenguinMeaN"
+REPO_NAME = "TankAI"
+
+VERSION_URL = f"https://raw.githubusercontent.com/{GITHUB_USER}/{REPO_NAME}/main/version.json"
+USER_DATA_URL = f"https://raw.githubusercontent.com/{GITHUB_USER}/{REPO_NAME}/main/users.json"
+CORE_URL = f"https://raw.githubusercontent.com/{GITHUB_USER}/{REPO_NAME}/main/core_engine.py"
+
+CURRENT_VERSION = "1.0.2"
